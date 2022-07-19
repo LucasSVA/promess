e91623eb759e5b0cf0c8b34418cdae45d716c7ec
LucasSVA <lucas.svanadze@gmail.com>
2022-07-19 21:21:13 +0200

    all 3 exercises completed

--- index.js
+++ index.js
@@ -1,28 +1,111 @@
-const promessFunction = () => {
+//---- ex -1
+
+// const seyHello = () => {
+//     return new Promise((resolve, reject) => {
+
+//         setTimeout(() => {
+//             const greet = "HEllo WORLD"
+
+//             resolve(greet)
+
+//         }, 2000)
+
+//     })
+
+// }
+
+
+// const waitForMyResults = async () => {
+
+//     const result = await promessFunction()
+//     console.log(result);
+
+// }
+
+// waitForMyResults()
+
+
+
+// ----ex 2
+
+
+// const weather = () => {
+//     return new Promise((resolve, reject) => {
+
+//         setTimeout(() => {
+//             const temperature = 2
+
+//             if (temperature <= 10) {
+//                 resolve("il fait froit")
+
+//             } else {
+//                 reject("il fait chaud")
+//             }
+
+//         }, 4000)
+//     })
+
+// }
+
+// const waitForWeather = async () => {
+
+//     try {
+//         const result = await weather()
+//         console.log(result);
+//     } catch (error) {
+//         console.log(error);
+
+//     }
+
+// }
+
+// waitForWeather()
+
+
+
+/// ------- ex - 3 
+
+
+
+
+const dishes = () => {
     return new Promise((resolve, reject) => {
 
+        setTimeout(() => {
+
+            resolve("j'ai fini la lessive")
+
+        }, 3000)
+
+    })
+
+}
+
+const loundry = () => {
+    return new Promise((resolve, reject) => {
 
         setTimeout(() => {
-            const greet = "HEllo world"
 
-            resolve(greet)
-
-
+            resolve("j'ai fini la vaisselle")
 
         }, 2000)
 
     })
 
-
 }
 
+const waitForHousework = async () => {
+    console.log("je commence la lessive");
+    const result1 = await dishes()
+    console.log(result1);
 
-const waitForMyResults = async () => {
+    console.log("je commence la vaisselle");
+    const result2 = await loundry()
+    console.log(result2);
 
-    const result = await promessFunction()
-    console.log(result);
+    console.log("J'ai fini de faire le ménage");
+
+
 
 }
-
-waitForMyResults()
-
+waitForHousework()
